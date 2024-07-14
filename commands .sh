8b20b3cdd29ea12f83a5afb31f2329177c967a40
Akshit <akshitahluwalia@gmail.com>
2024-07-14 05:00:56 +0000

    Updated

--- commands .sh
+++ commands .sh
@@ -1,11 +1,12 @@
-# Command Template
+eval "$(ssh-agent -s)" && ssh-add ~/.ssh/akshitahluwalia-personal
 
+# Command Template
 cd developer/trading-strategies/ && \
 source env/bin/activate && \
 python accumulator_v2.py \
 --token="eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIxMDQ1NzEiLCJqdGkiOiI2NjkzNTFmYmNiODFjYzFmODJjNTBkMmMiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaWF0IjoxNzIwOTMwODExLCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3MjA5OTQ0MDB9.tjC36lG0aRwEnKWWQcdW2cQuBYE77J8B-LRBxHYtcFA" \
 --lots="2" \
---option="CE" \
+--option="PE" \
 --underlying="NSE_INDEX|Nifty Bank" \
 --instrumentmasterpath="instruments.csv" \
 --expiry="2024-07-16"
